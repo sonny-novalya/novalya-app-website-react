1652fa8abf9d7c02ac792bed22c1b0ebb93a0382
Deepak ChroexTech <dkrana19942010@gmail.com>
2025-02-25 22:49:26 +0530

    Pagination design change are done

--- src/app/pages/protected/library/message/messageIndex.jsx
+++ src/app/pages/protected/library/message/messageIndex.jsx
@@ -84,6 +84,39 @@ const MessageIndex = () => {
               </List.Item>
             )}
           />
+          <div className='ctm-pagination flex items-center gap-[10px] justify-between border-t-[3px] border-[#E0E0E0] mt-5 pt-5'>
+            <div className='flex items-center gap-[10px]'>
+              <span className='text-[#000] text-[14px]'>Rows per pages</span>
+              <select className='border border-[#E0E0E0] px-2 py-1 text-[14px] rounded-[4px] text-black focus-visible:outline-none'>
+                <option>10</option>
+                <option>25</option>
+                <option>50</option>
+                <option>100</option>
+              </select>
+              <span className='text-[#8C8C8C] text-[14px]' >25 Rows selected</span>
+            </div>
+            <div className='pagination-wrap flex items-center gap-[10px] '>
+              <button className='flex items-center gap-[10px] font-medium text-[16px] leading-[1.5] text-[#404040] opacity-50 hover:opacity-100'>
+                <svg width="8" height="13" viewBox="0 0 8 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M0.37822 5.65751L6.03522 0.000514861L7.44922 1.41451L2.49922 6.36452L7.44922 11.3145L6.03522 12.7285L0.37822 7.07151C0.190749 6.88399 0.0854331 6.62968 0.0854331 6.36452C0.0854331 6.09935 0.190749 5.84504 0.37822 5.65751Z" fill="#404040"/>
+                </svg>
+                Previous
+              </button>
+              <ul className='pagination-list flex items-center gap-[10px] mx-1.5'>
+                <li className='active flex items-center justify-center w-6 h-6 border border-[#8C8C8C] font-medium text-[14px] rounded-[4px] hover:bg-[#0087FF] hover:text-white hover:border-[#008801]'><button>1</button></li>
+                <li className='flex items-center justify-center w-6 h-6 border border-[#8C8C8C] font-medium text-[14px] rounded-[4px] hover:bg-[#0087FF] hover:text-white !hover:border-[#008801]'><button>2</button></li>
+                <li className='flex items-center justify-center w-6 h-6 border border-[#8C8C8C] font-medium text-[14px] rounded-[4px] hover:bg-[#0087FF] hover:text-white !hover:border-[#008801]'><button>3</button></li>
+                <li className='flex items-center justify-center w-6 h-6 border border-[#8C8C8C] font-medium text-[14px] rounded-[4px] hover:bg-[#0087FF] hover:text-white !hover:border-[#008801]'><button>4</button></li>
+                <li className='flex items-center justify-center w-6 h-6 border border-[#8C8C8C] font-medium text-[14px] rounded-[4px] hover:bg-[#0087FF] hover:text-white !hover:border-[#008801]'><button>...</button></li>
+                <li className='flex items-center justify-center w-6 h-6 border border-[#8C8C8C] font-medium text-[14px] rounded-[4px] hover:bg-[#0087FF] hover:text-white !hover:border-[#008801]'><button>16</button></li>
+              </ul>
+              <button className='flex items-center gap-[10px] font-medium text-[16px] leading-[1.5] text-[#404040] opacity-50 hover:opacity-100'>Next 
+                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M7.15694 7.71163L1.49994 13.3686L0.0859375 11.9546L5.03594 7.00462L0.0859375 2.05463L1.49994 0.640625L7.15694 6.29763C7.34441 6.48515 7.44972 6.73946 7.44972 7.00462C7.44972 7.26979 7.34441 7.5241 7.15694 7.71163Z" fill="#404040"/>
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
