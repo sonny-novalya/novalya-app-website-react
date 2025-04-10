db01508538c482af9c3e608c1b7bb6bcdec28095
Jagmeet Singh <jagmeet.singh@chroextech.com>
2025-04-10 17:43:42 +0530

    Next Action component added

--- src/app/pages/protected/dashboard/TargetPromotion.jsx
+++ src/app/pages/protected/dashboard/TargetPromotion.jsx
@@ -1,7 +1,41 @@
+
 const TargetedPromotion = () => {
+    const actions = [
+        { action: "Import Facebook groups", completed: false, videoUrl: "https://www.youtube.com/watch?v=abc123" },
+        { action: "Add one person to CRM", completed: false, videoUrl: "https://www.youtube.com/watch?v=xyz789" },
+        { action: "Add one person to CRM", completed: false, videoUrl: "https://www.youtube.com/watch?v=def456" },
+    ]
+
     return (
-        <div className="bg-white shadow rounded-lg p-4 mt-5 border border-blue-400">
-            <h4 className="font-semibold text-sm">targeted promotion</h4>
+        <div className="bg-white shadow-lg rounded-lg p-4 mt-5">
+            <h4 className="font-semibold  mb-3">Next Actions</h4>
+            {actions.map((action, index) => (
+                <div key={index} className="flex items-center justify-between mb-4">
+                    <div className="flex items-center space-x-2">
+                        {/* <input
+                            type="checkbox"
+                            className="rounded-full border-gray-300"
+                            checked={action.completed}
+                            onChange={() => {
+                                setActions(prevActions =>
+                                    prevActions.map((prevAction, i) =>
+                                        i === index
+                                            ? { ...prevAction, completed: !prevAction.completed }
+                                            : prevAction
+                                    )
+                                );
+                            }}
+                        /> */}
+                        <div className="w-4 h-4 rounded-full border-2 border-blue-500 bg-white" />
+                        <span className="text-sm">{action.action}</span>
+                    </div>
+                    <a href={action.videoUrl} target="_blank" rel="noopener noreferrer">
+                        <button className="text-sm text-white bg-[#21BF7C] w-80 px-4 py-2 rounded hover:bg-green-600">
+                            Watch Video
+                        </button>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 };
