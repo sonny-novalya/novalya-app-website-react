646ba258aeffce196200db871f7401dcb1985750
Deepesh-chroextech <deepesh.sharma@chroextech.com>
2025-04-23 15:05:08 +0530

    CRM left

--- src/app/pages/protected/fb/crm/LeftSectionCrm.jsx
+++ src/app/pages/protected/fb/crm/LeftSectionCrm.jsx
@@ -1,8 +1,10 @@
-import { DndContext, 
-      closestCenter,
-    PointerSensor,
-    useSensor,
-    useSensors, } from "@dnd-kit/core";
+import {
+  DndContext,
+  closestCenter,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 import {
   arrayMove,
   SortableContext,
@@ -14,21 +16,22 @@ import { formatDate } from "../../../../../helpers/formatDate";
 import { t } from "i18next";
 import { useEffect, useRef, useState } from "react";
 import usefbCRM from "../../../../../store/fb/fbCRM";
+import { Button, Dropdown } from "antd";
+import { VerticalDotsIcon } from "../../../common/icons/icons";
 
-const SortableItem = ({ group, selectedGroup ,setSelectedGroup,getGroupById}) => {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useSortable({ id: group.id });
+const SortableItem = ({
+  group,
+  selectedGroup,
+  setSelectedGroup,
+  getGroupById,
+}) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: group.id });
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
-
 
   const isDragging = useRef(false);
   const startPos = useRef({ x: 0, y: 0 });
@@ -48,49 +51,95 @@ const SortableItem = ({ group, selectedGroup ,setSelectedGroup,getGroupById}) =>
     }
   };
 
-  const onClick = async(data) =>{
-    setSelectedGroup(data) 
-    
-    await getGroupById(data.id)
+  const onClick = async (data) => {
+    setSelectedGroup(data);
 
-   }
+    await getGroupById(data.id);
+  };
+
+  const DropdownMenu = ({ item }) => {
+    const [isDel, setIsDel] = useState(false);
+
+    const handleDelete = async (id) => {
+      if (isDel) {
+        setIsDel(false);
+      } else {
+        setIsDel(true);
+        delTime.current = setTimeout(() => {
+          setIsDel(false);
+        }, 3000);
+      }
+    };
+
+    return (
+      <div
+        className="bg-white rounded-md shadow-md p-2 relative z-99999"
+        onClick={(e) => e.stopPropagation()} // Prevent closing on click
+      >
+        <div
+          onClick={() => {}}
+          className="px-3 py-2 hover:bg-gray-100 cursor-pointer rounded relative z-99999"
+        >
+          Edit
+        </div>
+        <div
+          onClick={() => {}}
+          className="px-3 py-2 hover:bg-red-100 cursor-pointer rounded relative z-99999"
+        >
+          {isDel ? "Really?" : "Delete"}
+        </div>
+      </div>
+    );
+  };
 
   return (
-    <div >
-
-    
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onClick={() => onClick(group)}
-      className={`flex items-center space-x-2 px-4 py-2 mb-2 rounded-md cursor-pointer border bg-white hover:bg-gray-100 ${
-        selectedGroup?.id === group.id ? "border-[#0087FF]" : "border-[#E6F1FB]"
-      }`}
-      
-    >
-      <div  className="w-10 h-9 rounded-full flex items-center justify-center font-bold text-white bg-blue-500 uppercase">
-        {group.name
-          .split(" ")
-          .map((word) => word[0])
-          .join("")
-          .slice(0, 2)}
-      </div>
-      <div className="flex flex-col items-center w-full">
-        <div className="flex items-center justify-between w-full">
-          <span className="capitalize">{group.name}</span>
-          <span className="text-xl text-gray-400">⋮</span>
+    <div className="relative">
+      <div
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onClick={() => onClick(group)}
+        className={`flex items-center space-x-2 px-4 py-2 mb-2 rounded-md cursor-pointer border bg-white hover:bg-gray-100 ${
+          selectedGroup?.id === group.id
+            ? "border-[#0087FF]"
+            : "border-[#E6F1FB]"
+        }`}
+      >
+        <div className="w-10 h-9 rounded-full flex items-center justify-center font-bold text-white bg-blue-500 uppercase">
+          {group.name
+            .split(" ")
+            .map((word) => word[0])
+            .join("")
+            .slice(0, 2)}
         </div>
-        <div className="flex items-center justify-between w-full">
-          {/* <span className="text-sm text-gray-600">20 leads</span> */}
-          <span className="text-sm text-gray-400">{formatDate(group.createdAt)}</span>
+        <div className="flex flex-col items-center w-full">
+          <div className="flex items-center justify-between w-full">
+            <span className="capitalize">{group.name}</span>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            {/* <span className="text-sm text-gray-600">20 leads</span> */}
+            <span className="text-sm text-gray-400">
+              {formatDate(group.createdAt)}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-
+      <div className="absolute right-[10px] top-[10px]">
+        <Dropdown
+          overlay={<DropdownMenu />}
+          trigger={["click"]}
+          placement="bottomRight"
+        >
+          <Button
+            type="text"
+            icon={<VerticalDotsIcon />}
+            className="!text-[#808183] !h-9 btn-hover"
+          />
+        </Dropdown>
+      </div>
     </div>
   );
 };
@@ -103,37 +152,31 @@ const LeftSectionCrm = ({
   onDragEnd,
   error,
   isLoading,
-  reorderCRMGroupsFB
+  reorderCRMGroupsFB,
 }) => {
   const [localGroups, setLocalGroups] = useState(groups);
   const startIndexRef = useRef(null);
-  const {getGroupById}=usefbCRM()
-
+  const { getGroupById } = usefbCRM();
 
   const sensors = useSensors(useSensor(PointerSensor));
 
-  useEffect(() => { 
-    setLocalGroups(groups)
-  }, [groups])
+  useEffect(() => {
+    setLocalGroups(groups);
+  }, [groups]);
 
- 
-  
   const handleDragStart = (event) => {
     const { active } = event;
     const index = localGroups.findIndex((g) => g.id === active.id);
     startIndexRef.current = index;
   };
 
-  const handleDragEnd = async(event) => {
+  const handleDragEnd = async (event) => {
     const { active, over } = event;
 
     if (!over) return;
 
-   
-
     const oldIndex = startIndexRef.current;
     const newIndex = localGroups.findIndex((g) => g.id === over.id);
-
 
     // ✅ Only reorder if moved
     if (oldIndex !== newIndex) {
@@ -142,7 +185,7 @@ const LeftSectionCrm = ({
       onDragEnd?.(newOrder);
     }
 
-    await reorderCRMGroupsFB({destination: newIndex,source: oldIndex})
+    await reorderCRMGroupsFB({ destination: newIndex, source: oldIndex });
 
     startIndexRef.current = null; // cleanup
   };
@@ -163,18 +206,20 @@ const LeftSectionCrm = ({
     );
   }
 
-
   return (
-    <DndContext collisionDetection={closestCenter}    sensors={sensors}  onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+    <DndContext
+      collisionDetection={closestCenter}
+      sensors={sensors}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
       <SortableContext
         items={localGroups.map((g) => g.id)}
         strategy={verticalListSortingStrategy}
       >
         <div className="mt-4">
           {localGroups
-            ?.filter((g) =>
-              g.name.toLowerCase().includes(search.toLowerCase())
-            )
+            ?.filter((g) => g.name.toLowerCase().includes(search.toLowerCase()))
             ?.map((group) => (
               <SortableItem
                 key={group.id}
@@ -189,9 +234,6 @@ const LeftSectionCrm = ({
     </DndContext>
   );
 };
-
-
-
 
 // LeftSectionCrm.propTypes = {
 //     groups: PropTypes.array.isRequired,
