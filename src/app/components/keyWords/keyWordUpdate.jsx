098415d7ec1791dedb5e39f057929eddac95d6cc
Deepak ChroexTech <dkrana19942010@gmail.com>
2025-04-25 06:54:58 +0530

    Keywords and ai comment changs are done

--- src/app/components/keyWords/keyWordUpdate.jsx
+++ src/app/components/keyWords/keyWordUpdate.jsx
@@ -60,23 +60,23 @@ const KeywordUpdatePop = ({containerRef}) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[#00000047] h-screen">
     <div ref={containerRef} className="bg-white px-6 py-5 rounded-[10px] max-w-[700px] mx-auto w-full relative max-h-[90vh] overflow-auto">
-        <h3 className="text-[20px] font-medium mb-5">Update Keyword</h3>
+        <h3 className="text-[20px] font-[500] mb-2.5">Update Keyword</h3>
         <div className="border border-[#DCDCDC] rounded-[10px] p-5">
-            <div className="mb-4">
-                <label className="block text-sm font-medium mb-2">Name of your Keywords</label>
+            <div className="mb-3">
+                <label className="block text-sm font-[500] mb-1.5">Name of your Keywords</label>
                 <input onChange={(e)=>setData({...data,name:e.target.value})}  value={data.name} className="w-full border border-[#DCDCDC] bg-[rgb(217_217_217_/_10%)] rounded-[4px] px-4 py-2 leading-5 text-[rgba(0,0,0,0.75)] font-light placeholder-[text-[rgba(0,0,0,0.4)]" type="text" name="" placeholder="Name"/>
             </div>
-            <div className="mb-4">
-                <label className="block text-sm font-medium mb-2">Positive Keywords</label>
+            <div className="mb-3">
+                <label className="block text-sm font-[500] mb-1.5">Positive Keywords</label>
                 <input onChange={(e)=>handleKeywordsEntery(e.target.value,1)}  value={ConvertArray(data?.positive_keyword || [])} className="w-full border border-[#DCDCDC] bg-[rgb(217_217_217_/_10%)] rounded-[4px] px-4 py-2 leading-5 text-[rgba(0,0,0,0.75)] font-light placeholder-[text-[rgba(0,0,0,0.4)]" type="text" name="" placeholder="Enter keyword, Separated by commas "/>
             </div>
-            <div>
-                <label className="block text-sm font-medium mb-2">Negative Keywords</label>
+            <div className='mb-3'>
+                <label className="block text-sm font-[500] mb-1.5">Negative Keywords</label>
                 <input onChange={(e)=>handleKeywordsEntery(e.target.value,0)}  value={ConvertArray(data?.negative_keyword || [])}   className="w-full border border-[#DCDCDC] bg-[rgb(217_217_217_/_10%)] rounded-[4px] px-4 py-2 leading-5 text-[rgba(0,0,0,0.75)] font-light placeholder-[text-[rgba(0,0,0,0.4)]" type="text" name="" placeholder="Enter keyword, Separated by commas "/>
             </div>
         </div>
         <div className="flex justify-end mt-4">
-            <button className="bg-[#0087FF] text-white text-sm rounded-[6px] px-12 py-3 cursor-pointer" disabled={actionLoader} onClick={()=>onsubmit()}>{actionLoader?<Spin size="small" style={{color:"white"}} />:"Submit"}</button>
+            <button className="bg-[#0087FF] text-white text-sm rounded-[6px] px-12 py-3 w-[154px] cursor-pointer" disabled={actionLoader} onClick={()=>onsubmit()}>{actionLoader?<Spin size="small" style={{color:"white"}} />:"Submit"}</button>
         </div>
     </div>
   </div>
