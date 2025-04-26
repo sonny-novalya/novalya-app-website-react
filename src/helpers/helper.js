e9679aab4aba6bc0fc55dc448f9fe6e2aaba1fdc
Deepesh-chroextech <deepesh.sharma@chroextech.com>
2025-04-26 18:47:57 +0530

    feat:level commission

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -112,3 +112,11 @@ function getCookie(cname) {
   }
   return "";
 }
+
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+  
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  
+  return date.toLocaleDateString('en-US', options);
+}
