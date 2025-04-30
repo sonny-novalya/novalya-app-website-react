4d6877b19ef2b8c35d1a2de6853704b71fc8cc94
Jagmeet Singh <jagmeet.singh@chroextech.com>
2025-04-30 09:27:38 +0530

    fixes

--- src/app/components/modal/fb/prospection/ConfirmationModal/Filters.jsx
+++ src/app/components/modal/fb/prospection/ConfirmationModal/Filters.jsx
@@ -17,9 +17,8 @@ const Filters = ({ gender, keyword, handleOpen }) => {
       <div className="flex justify-between border border-[#00000014] rounded-md p-4">
         <div className="flex flex-col flex-1 pr-4 space-y-2">
           <h3 className="font-medium">{t("prospecting.Gender")}</h3>
-          <p className="w-full text-center border border-[#00000014] rounded-md p-2">
-            {/* {gender} */}
-            {t(`prospecting.${gender.charAt(0).toUpperCase() + gender.slice(1)}`)}
+          <p className="w-full text-center border border-[#00000014] rounded-md p-2 capitalize">
+            {gender}
           </p>
         </div>
         <div className="border-l-2 border-[#00000014] px-5 space-y-2 flex-1">
