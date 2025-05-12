1189f6b2e7d08af61f178c0b0f0dae4453a2b573
Jagmeet Singh <jagmeet.singh@chroextech.com>
2025-05-12 17:18:43 +0530

    removing my stats button

--- src/app/pages/protected/affiliate/AffiliateDashboardNew/AffiliateDashboard.jsx
+++ src/app/pages/protected/affiliate/AffiliateDashboardNew/AffiliateDashboard.jsx
@@ -24,9 +24,10 @@ const AffiliateDashboard = () => {
                 {/* Header */}
                 <div className="flex justify-between items-center mb-6">
                     <h2 className="text-2xl font-medium text-[#000407]">Affiliate</h2>
-                    <button className="bg-[#0087FF] hover:bg-[#005199] text-white px-4 py-2 rounded-md transition-colors">
+                    {/* <button className="bg-[#0087FF] hover:bg-[#005199] text-white px-4 py-2 rounded-md transition-colors">
                         My Stats
-                    </button>
+                    </button> */}
+                    <div></div>
                 </div>
 
                 {
